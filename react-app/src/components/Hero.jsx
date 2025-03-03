5a5f60330a30f8764bd97d42752255e14326efa5
Lee J Noel <lee.j.noel.ctr@socom.mil>
2025-03-03 14:48:09 -0500

    updated refactor

---
+++ react-app/src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Hero.css'; // will create this file later for hero-specific styles
+
+const Hero = () => {
+    return (
+        <section id = "hero-section">
+            <div> id = "profile-img"</div>
+            <h2>Lee Joey Noel</h2>
+            <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+        </section>
+    );
+};
+
+export default Hero;
